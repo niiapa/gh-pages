b2b430907e722bbab8b06d7c9f3a9335043ceb6e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-12 13:53:59 +0100

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+const Navbar = () => {
+    const links = [
+        {
+            name: 'Home',
+            url: '/',
+        },
+        {
+            name: 'Projects',
+            url: 'projects'
+        }
+    ];
+    
+    return (
+        <nav>
+            <ul>
+                {
+                    links.map((link, index) => (
+                        <li key={index}>
+                            <Link to={link.url} activeClassName='active-link'>
+                                {link.name}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
