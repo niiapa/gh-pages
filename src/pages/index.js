f2171e1954d2e30c4e4da59415282050fc243b29
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-12 15:51:43 +0100

    Added title to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo';
 
 const Index = () => (
     <Layout>
-        <SEO title=""/>
+        <SEO title="Home"/>
         
         <div className="intro-box">
             <div className='avatar'>
