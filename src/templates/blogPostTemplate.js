ffd8a521a25b07b3e4ab8f5ba5487800ce895aa6
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-12 14:01:48 +0100

    Create blogPostTemplate.js

---
+++ src/templates/blogPostTemplate.js
@@ -0,0 +1,34 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import Layout from '../components/layout';
+
+const BlogPostTemplate = ({ data }) => {
+    const { markdownRemark: { frontmatter, html } } = data;
+    
+    return (
+        <Layout>
+            <div className="blog-post">
+                <h1>{frontmatter.title}</h1>
+                <time dateTime={frontmatter.date}>{frontmatter.formattedDate}</time>
+                
+                <div className='blog-post-content' dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+        </Layout>
+    );
+};
+
+export const pageQuery = graphql`
+    query($path: String!) {
+        markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            frontmatter {
+                date
+                formattedDate: date(formatString: "DD MMMM, YYYY")
+                path
+                title
+            }
+        }
+    }
+`;
+
+export default BlogPostTemplate;
