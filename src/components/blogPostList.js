6c72a92b7f57962e7b4d8766750b03f195e36c96
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-12 14:01:42 +0100

    Create blogPostList.js

---
+++ src/components/blogPostList.js
@@ -0,0 +1,62 @@
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import React from 'react';
+
+const BlogPostList = () => {
+    const { allMarkdownRemark: { edges } } = useStaticQuery(
+        graphql`
+            query {
+                allMarkdownRemark(
+                    sort: {
+                        order: DESC,
+                        fields: [frontmatter___date]
+                    }
+                ) {
+                    edges {
+                        node {
+                            id
+                            excerpt(pruneLength: 250)
+                            frontmatter {
+                                date
+                                formattedDate: date(formatString: "DD MMMM, YYYY")
+                                path
+                                title
+                            }
+                        }
+                    }
+                }
+            }
+        `
+    );
+    
+    return (
+        <div className='blog-post-list'>
+            {
+                edges.map((edge, index) => {
+                    const post = edge.node.frontmatter;
+                    
+                    return (
+                        <Link to={post.path}>
+                            <p key={index}>
+                                <h3>
+                                    {post.title}
+                                </h3>
+                
+                                <p>
+                                    {edge.node.excerpt}
+                                </p>
+                
+                                <small>
+                                    <time dateTime={post.date}>
+                                        {post.formattedDate}
+                                    </time>
+                                </small>
+                            </p>
+                        </Link>
+                    )
+                })
+            }
+        </div>
+    );
+};
+
+export default BlogPostList;
