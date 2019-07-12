e515d62bca2fb4fca87daae376eef5b8e4d9c00f
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-07-12 14:01:31 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,70 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Nii Apa`,
+        description: `A mini-site about me`,
+        author: `Nii Apa Abbey`,
+        twitterHandle: '@niiapa',
+        socials: [
+            {
+                name: 'github',
+                url: 'https://github.com/niiapa'
+            },
+            {
+                name: 'twitter',
+                url: 'https://twitter.com/niiapa'
+            },
+            {
+                name: 'instagram',
+                url: 'https://instagram.com/niiapa'
+            },
+            {
+                name: 'spotify',
+                url: 'https://open.spotify.com/user/niiapa?si=oESrQQt-QuOSkawLRfFVZw'
+            },
+            {
+                name: 'wordpress',
+                url: 'https://abbeyabyss.wordpress.com'
+            },
+            {
+                name: 'soundcloud',
+                url: 'https://soundcloud.com/dj-abbey-gh'
+            },
+        ]
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `${__dirname}/src/markdown-pages`,
+            },
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#141414`,
+                theme_color: `#141414`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-sass`
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
