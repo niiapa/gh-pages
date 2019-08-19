637bd06dcd10c884be4178b39ed6236ee601c666
Nii Apa Abbey <abbeyniiapa@gmail.com>
2019-08-19 23:45:51 +0100

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Avatar from '../components/avatar';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const tech = [
+    'javascript',
+    'reactjs',
+    'vuejs',
+    'redux',
+    'graphql',
+    'firebase',
+    'php',
+    'laravel',
+    'java',
+    'mysql'
+];
+
+const About = () => (
+    <Layout>
+        <SEO title="About"/>
+        
+        <div className='about'>
+            <div className='center'>
+                <div className='avatar'>
+                    <Avatar/>
+                </div>
+                
+                <h2>Nii Apa Abbey</h2>
+                <small><i>Software Engineer</i></small>
+                
+                <div style={{ display: 'flex',  flexWrap: 'wrap', flex: 1, justifyContent: 'center'}}>
+                    {
+                        tech.map((tech, index) => (
+                            <span key={index} className='meta'>{tech}</span>
+                        ))
+                    }
+                </div>
+            </div>
+            
+        </div>
+    </Layout>
+);
+
+export default About;
