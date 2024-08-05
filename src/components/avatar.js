a47ac045fcdfb24c3a41c252b831ee4dd82dafc0
Nii Apa Abbey <abbeyniiapa@gmail.com>
2024-08-06 00:09:58 +0100

    Use StaticImage for avatar

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,28 +1,16 @@
-import { graphql, useStaticQuery } from 'gatsby';
-import { GatsbyImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 import React from 'react';
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 const Avatar = () => {
-    const data = useStaticQuery(graphql`{
-  placeholderImage: file(relativePath: {eq: "avatar.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(width: 300, height: 300, layout: CONSTRAINED)
-    }
-  }
-}`);
-    
-    return <GatsbyImage image={data.placeholderImage.childImageSharp.gatsbyImageData} alt="Avatar" />;
+    return (
+        <StaticImage
+            src="../images/avatar.jpg"
+            alt="Avatar"
+            width={300}
+            height={300}
+            layout="constrained"
+        />
+    );
 };
 
 export default Avatar;
